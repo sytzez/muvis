195a5e100b785f5b1233b680130988949824be52
sytzez <sytzez@gmail.com>
2019-10-11 13:46:31 +0200

    Better error screen

--- components/ErrorView.js
+++ components/ErrorView.js
@@ -2,16 +2,44 @@ const ErrorView = (() => {
   'use strict';
   const e = React.createElement;
   const br = (key) => e('br', { key });
-  return ({ fix }) => e('div', { className: 'bigError' }, [
-    'An unexpected error has crashed the app!', br(0),
-    'You can try to revert to the previous state by clicking ',
-    e('button',{
-      onClick: () => {
-        store.dispatch({ type: 'UNDO' });
-        fix();
-      },
-      key: 1,
-    }, 'undo'), br(2),
-    // 'Or you can download the project's state as a JSON file', // TODO
-  ]);
+  return class extends React.Component {
+    state = {
+      url: '',
+    };
+
+    getLink() {
+      const json = jsonSaver(store.getState());
+      const file = new File([json], 'project.json', {
+        type: 'application/json',
+      });
+      const url = window.URL.createObjectURL(file);
+      this.setState({ url });
+    }
+
+    render() {
+      const { fix } = this.props;
+      const { url } = this.state;
+      return e('div', { className: 'bigError' }, [
+        'An unexpected error has crashed the app!', br(0),
+        'You can try to revert to the previous state by clicking ',
+        e('button',{
+          onClick: () => {
+            store.dispatch({ type: 'UNDO' });
+            fix();
+          },
+          key: 1,
+        }, 'Undo'), br(2),
+        'Or you can download the project as a JSON file to load again later: ',
+        e('button',{
+          onClick: this.getLink.bind(this),
+          key: 3,
+        }, 'Get link'),
+        url !== '' ? e('a', {
+          href: url,
+          download: true,
+          key: 4,
+        }, 'Download link') : null,
+      ]);
+    }
+  };
 })();
